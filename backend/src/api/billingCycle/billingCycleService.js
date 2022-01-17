8792b1fc322f50af3b912970e8b877c8e2a75e68
diego <diegopgcds10@gmail.com>
2022-01-17 16:45:19 -0300

    Testando a API (Postman)

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,6 +1,24 @@
 const BillingCycle = require('./billingCycle')
 
-BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.route('get', (req, res, next) => {
+
+    BillingCycle.find({}, (err, docs) => {
+
+        if(!err) {
+
+            res.json(docs)
+
+        } else {
+
+            res.status(500).json({errors: [error]})
+
+        }
+
+    })
+
+})
+
+BillingCycle.methods(['post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
 module.exports = BillingCycle

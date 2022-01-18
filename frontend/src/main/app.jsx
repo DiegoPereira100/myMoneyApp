8a38088a94e86d4e091403ce1ce0880a4df6bb91
diego <diegopgcds10@gmail.com>
2022-01-18 17:04:10 -0300

    Componente Menu #01

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,9 +2,11 @@ import '../common/template/dependencies'
 import React from "react";
 
 import Header from '../common/template/header';
+import SideBar from '../common/template/sidebar';
 
 export default props => (
-    <div className="wrapper">
+    <div className="skin-blue">
         <Header></Header>
+        <SideBar></SideBar>
     </div>
 )
